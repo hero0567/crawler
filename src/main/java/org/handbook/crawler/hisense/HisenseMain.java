e61c4a5bee1d2bb01f95b05cdc7fed70ca76e1cc
hero0567@163.com <hero0567@163.com>
2016-01-21 21:08:29 +0800

    start heisin

---
+++ src/main/java/org/handbook/crawler/hisense/HisenseMain.java
@@ -0,0 +1,42 @@
+package org.handbook.crawler.hisense;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class HisenseMain {
+    public static void main(String[] args) throws Exception {
+        String crawlStorageFolder = "./crawler";
+        int numberOfCrawlers = 1;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+//        config.setPolitenessDelay(500);
+//        config.setMaxDepthOfCrawling(3);
+//        config.setUserAgentString("");
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+//        controller.addSeed("http://mall.midea.com");
+        controller.addSeed("http://www.hisense.com/fwzx/xzzx/smsqd/");
+        
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(HisenseCrawler.class, numberOfCrawlers);
+    }
+}
--- src/main/java/org/handbook/crawler/hisense/TransferToDB.java
+++ src/main/java/org/handbook/crawler/hisense/TransferToDB.java
@@ -1,17 +1,14 @@
-package org.handbook.crawler.shuomingshuku;
+package org.handbook.crawler.hisense;
 
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.Statement;
+
 
 public class TransferToDB {
 	public static void main(String[] args) throws Exception {
@@ -25,7 +22,7 @@ public class TransferToDB {
 			conn.setAutoCommit(false);
 			Statement stmt = conn.createStatement();
 			
-			File data = new File("./shuomingshuku");
+			File data = new File("./siemens");
 			File[] fs = data.listFiles();
 			for (int i = 0; i < fs.length; i++) {
 				System.out.println(fs[i].getAbsolutePath());
@@ -46,35 +43,28 @@ public class TransferToDB {
 			InputStreamReader read = new InputStreamReader(new FileInputStream(file), "UTF-8");
 			BufferedReader reader = new BufferedReader(read);
 			String line;
-			String url = null, name = null, company = null, filetype = null;
-			String filesize = null, updatetime = null, filemd5 = null;
-			String downloadcount = null, downloadlink1 = null, downloadlink2 = null;
-			String sql = "insert into crawler_data  (url, name, company, filetype, filesize,"
-					+ " updatetime, filemd5, downloadcount,downloadlink1,downloadlink2)  values(";
+			String url = null, title = null, pdfurl = null, jpgURLs = null;
+			String info = null;
+			String sql = "insert into crawler_siemens (url, title, pdfurl, jpgurl, info)  values(";
 			StringBuffer buffer = new StringBuffer();
 			while ((line = reader.readLine()) != null) {
 				String[] splits = line.split(";");
-				if (splits.length < 10) {
+				if (splits.length < 5) {
 					System.out.println("Skip :" + line);
 					continue;
 				}
+//				b.append(url).append(";").append(title.trim()).append(";").append(pdfURL).append(";")
+//				.append(jpgURLs.toString()).append(";").append(headers.toString()).append(";");
 				url = splits[0].trim();
-				name = splits[1].trim().replaceAll("\"", "\\\\\"");
-				company = splits[2].trim().replaceAll("\"", "\\\\\"");
-				filetype = splits[3].trim().replaceAll("\"", "\\\\\"");
-				filesize = splits[4].trim();
-				updatetime = splits[5].trim();
-				filemd5 = splits[6].trim();
-				downloadcount = splits[7].trim();
-				downloadlink1 = splits[10].trim();
-				downloadlink2 = splits[10].trim().replaceAll("\"", "\\\\\"");
+				title = splits[1].trim();
+				pdfurl = splits[2].trim();				
+				jpgURLs = splits[3].trim();				
+				info = splits[4].trim();
 
 				buffer.setLength(0);
-				buffer.append(sql).append("\"").append(url).append("\",\"").append(name).append("\",\"").append(company)
-						.append("\",\"").append(filetype).append("\",\"").append(filesize).append("\",\"").append(updatetime)
-						.append("\",\"").append(filemd5).append("\",\"").append(downloadcount).append("\",\"")
-						.append(downloadlink1).append("\",\"").append(downloadlink2).append("\")");
-//
+				buffer.append(sql).append("\"").append(url).append("\",\"").append(title).append("\",\"").append(pdfurl)
+						.append("\",\"").append(jpgURLs).append("\",\"").append(info).append("\")");
+
 //				System.out.println("Data:" + line);
 //				System.out.println("SQL:" + buffer.toString());
 //				stmt.addBatch(sql);
